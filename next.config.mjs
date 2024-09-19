22a8fd84d065c77fb0c397795251ffbf4728a8d6
AnnaFutryk <anna.futryk@gmail.com>
2024-09-19 16:25:27 +0300

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  basePath: "/sifu",
+  assetPrefix: "/sifu/",
+  trailingSlash: true,
+};
 
-export default nextConfig;
+module.exports = nextConfig;
