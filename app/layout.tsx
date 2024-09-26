b506cdd908b5ee0c620f420ab80e32b37ee434a6
AnnaFutryk <anna.futryk@gmail.com>
2024-09-26 22:26:57 +0300

    added footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "../components/modules/Header";
+import Footer from "../components/modules/Footer";
 
 export const metadata: Metadata = {
   title: "SIFU",
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body style={{ overflowX: "hidden" }}>
         <Header/>
         <main>{children}</main>
+        <Footer/>
       </body>
     </html>
   );
---
+++ components/modules/Footer/index.tsx
@@ -0,0 +1,12 @@
+import cls from "./styles.module.scss";
+
+const Footer = () => {
+  return (
+    <footer className={cls.footer}>
+      <hr className={cls.hr} />
+      <p className={cls.txt}>LOREM IPSUM - ALL RIGHTS RESERVED</p>
+    </footer>
+  );
+};
+
+export default Footer;
